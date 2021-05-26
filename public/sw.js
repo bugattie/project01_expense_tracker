4cc327765028502ddd7b6dfe7cb8b2d8c180a435
bugattie <m.mohsinbarkaati@gmail.com>
2021-05-26 10:58:56 +0500

    Cache url updated.

--- public/sw.js
+++ public/sw.js
@@ -10,6 +10,11 @@ const urlsToCache = [
   "/manifest.json",
   "/favicon.ico",
   "/logo192.png",
+  "/static/css/main.eca65a5b.chunk.css",
+  "/static/js/2.a873e7b4.chunk.js",
+  "/static/js/main.a3fc11af.chunk.js",
+  "https://fonts.googleapis.com/css?family=Lato&display=swap",
+  "https://fonts.gstatic.com/s/lato/v17/S6uyw4BMUTPHjx4wXg.woff2",
 ];
 
 self.addEventListener("install", (event) => {
